5665e6b1343f716510cd58c11b5552b727aa73ca
Nugie kurniawan <nugiebinhar@gmail.com>
2025-11-23 15:37:24 +0700

    controller:membuat endpoint untuk update data dht untuk di pasangkan di arduino ide

--- controllers/dht22Controller.js
+++ controllers/dht22Controller.js
@@ -1,12 +1,26 @@
 import Dht22 from "../models/Dht22.js";
-export const index = async(req, res) => {
+export const index = async (req, res) => {
     try {
         const dht = await Dht22.findUnique({
             where: {id: 1},
         });
         return res.status(200).json(dht);
-
     } catch (error) {
-        res.status(404).json({message:"tidak ada data"})
+        res.status(404).json({message: "tidak ada data"});
+    }
+};
+
+export const update = async (req, res) => {
+    try {
+        const {temp, humd} = req.params;
+
+        const update = await Dht22.update({
+            where: {id: 1},
+            data: {temperature: parseFloat(temp), humidity: parseFloat(humd)},
+        });
+
+        return res.status(200).json(update);
+    } catch (error) {
+        return res.status(400).json({message: "gagal : " + error});
     }
 };
--- routes/smartHome.route.js
+++ routes/smartHome.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { index, update } from "../controllers/smartHomeController.js";
-import { index as indexDht } from "../controllers/dht22Controller.js";
+import { index as indexDht,update as updateDht } from "../controllers/dht22Controller.js";
 const route = Router();
 
 // untuk route smartHome
@@ -8,6 +8,9 @@ route.get("/status",index);
 route.put("/smartHome/update",update)
 
 // untuk route dht
+// untuk mendapatkan data dht 22
 route.get("/dht22/all",indexDht)
 
+// untuk mengupdate data dht 22
+route.get("/dht22/update/:temp/:humd",updateDht);
 export default route
